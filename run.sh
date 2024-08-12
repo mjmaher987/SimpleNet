5ff4bb358b2424a06bdd52842cd8aeaf82ec34dd
Mohammad Javad Maheronnaghsh <mjmaher987@gmail.com>
2024-08-12 05:30:17 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-datapath=/data4/MVTec_ad
-datasets=('screw' 'pill' 'capsule' 'carpet' 'grid' 'tile' 'wood' 'zipper' 'cable' 'toothbrush' 'transistor' 'metal_nut' 'bottle' 'hazelnut' 'leather')
+# datapath=/data4/MVTec_ad
+datapath=/content
+# datasets=('screw' 'pill' 'capsule' 'carpet' 'grid' 'tile' 'wood' 'zipper' 'cable' 'toothbrush' 'transistor' 'metal_nut' 'bottle' 'hazelnut' 'leather')
+datasets=('pill')
 dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
 
 python3 main.py \
---gpu 4 \
+--gpu 1 \
 --seed 0 \
 --log_group simplenet_mvtec \
 --log_project MVTecAD_Results \
@@ -16,7 +18,7 @@ net \
 --pretrain_embed_dimension 1536 \
 --target_embed_dimension 1536 \
 --patchsize 3 \
---meta_epochs 40 \
+--meta_epochs 15 \
 --embedding_size 256 \
 --gan_epochs 4 \
 --noise_std 0.015 \
